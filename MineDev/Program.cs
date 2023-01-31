e211e6581b428d9278bf1b010d4261cf83fe8a95
Dim <dok@rmiac18.ru>
2023-01-31 09:51:58 +0400

    Add strategy pattern

--- MineDev/Program.cs
+++ MineDev/Program.cs
@@ -1,6 +1,18 @@
-﻿Console.WriteLine("Start");
+﻿using MineDevLibrary;
+using System;
+Console.WriteLine("Start");
 
-//add code here
+B a = (B)(new A());
+
+Delegates.SimpleDelegateUse();
 
 Console.WriteLine("The end");
 Console.ReadLine();
+
+
+public class A 
+{
+
+}
+
+public class B : A { }
---
+++ MineDevLibrary/VarianceExamples.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineDevLibrary
+{
+    /// <summary>
+    /// примеры визуализирующие вариантности
+    /// ковариантность - перенос от предка к потомку (возможность использовать потомка там где требуется предок)
+    /// контрвариантность - перенос от потомка к предку (возможность исопльзовтаь предка там где нужен потомок) 
+    /// инвариантность - наследование не переносится (можно использовать только то что требуется)
+    /// </summary>
+    internal class VarianceExamples
+    {
+        //иерархия наследования
+        //Instrument -> Guitar
+        private class Instrument { }
+        private class Guitar  : Instrument{ }
+
+        //вариантность - это перенос не между прямыми типами, а от типов к производным (т.е. нгапример к ообобщениям) 
+        //ниже два листа и есть производные типы
+        private List<Instrument> instruments= new List<Instrument>();
+        private List<Guitar> guitars= new List<Guitar>();
+
+        //ковариантность позволила бы передать сюда гиатры List<Guitar> (ну лист инвариантен)
+        void CoVariance(List<Instrument> instruments) { }
+
+        public void Example()
+        {
+           // CoVariance(guitars); - ошибка
+        }
+    }
+
+
+
+
+}
